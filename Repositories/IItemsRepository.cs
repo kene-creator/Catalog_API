daa688a8beea796dd07e103ea9597ecb8053ba49
Kene <kayyungx@gmail.com>
2023-08-03 23:37:54 +0100

    add item repository interface

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -11,5 +11,7 @@ namespace Catalog_API.Repositories
          Item? GetItem(Guid id);
         IEnumerable<Item> GetItems();
         void CreateItem(Item item);
+        void UpdateItem(Item item);
+        void DeleteItem(Guid id);
     }
 }
