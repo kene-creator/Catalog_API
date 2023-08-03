e7ad57370ed86e1e66a6ca4065462e7f5ecf3a89
Kene <kayyungx@gmail.com>
2023-08-03 23:34:40 +0100

    update program file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 using Catalog_API.Repositories;
+using Catalog_API.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSingleton<IItemsRepository, InMemoryItemsRepository >();
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+// Add services to the container.
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+builder.Services.AddSingleton<IMongoClient>(sp =>
+        {
+            var configuration = sp.GetRequiredService<IConfiguration>().GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            return new MongoClient(configuration?.ConnectionString);
+        });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
