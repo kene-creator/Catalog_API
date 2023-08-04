9b442453ea7850f9d3643c936f9487a8e8d0d926
Kene <kayyungx@gmail.com>
2023-08-04 17:32:34 +0100

    implement update and delete methods

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -13,6 +13,7 @@ namespace Catalog_API.Repositories
         private const string databaseName = "catalog";
         private const string collectionName = "items";
         private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
         public MongoDbItemsRepository(IMongoClient mongoClient)
         {
             IMongoDatabase database = mongoClient.GetDatabase(databaseName);
@@ -25,12 +26,14 @@ namespace Catalog_API.Repositories
 
         public void DeleteItem(Guid id)
         {
-            throw new NotImplementedException();
+             var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
         }
 
-        public Item? GetItem(Guid id)
+        public Item GetItem(Guid id)
         {
-            throw new NotImplementedException();
+           var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
         }
 
         public IEnumerable<Item> GetItems()
@@ -40,7 +43,8 @@ namespace Catalog_API.Repositories
 
         public void UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+            itemsCollection.ReplaceOne(filter, item);
         }
     }
 }
