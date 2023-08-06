370f1bb6645dc6562febfe41416960a90cadd63e
Kene <kayyungx@gmail.com>
2023-08-06 20:56:12 +0100

    configure database health check

--- Program.cs
+++ Program.cs
@@ -7,18 +7,32 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongoDbSettings.ConnectionString,
+        name: "mongodb",
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[] { "ready" }
+    );
 
 // Add services to the container.
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddSingleton<IMongoClient>(sp =>
         {
-            var configuration = sp.GetRequiredService<IConfiguration>().GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-            return new MongoClient(configuration?.ConnectionString);
+            // var configuration = sp.GetRequiredService<IConfiguration>().GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            return new MongoClient(mongoDbSettings?.ConnectionString);
         });
 
-builder.Services.AddControllers();
+        
+
+builder.Services.AddControllers(options => {
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -37,5 +51,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
