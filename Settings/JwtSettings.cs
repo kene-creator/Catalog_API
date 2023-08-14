3c3f560b42232c74e019d9af24a2a1ffe233fa04
Kene <kayyungx@gmail.com>
2023-08-14 12:37:17 +0100

    Add jwt settings class

---
+++ Settings/JwtSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_API.Settings
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+    }
+}
