ac03032714b4401c85892d4a7bed32b720fb1603
Kene <kayyungx@gmail.com>
2023-08-30 16:27:37 +0100

    Add token dto

---
+++ Dtos/TokenDtos/RefreshTokenRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog_API.Dtos.TokenDtos;
+
+public record RefreshTokenRequestDto
+{
+    public string RefreshToken { get; set; }
+}
---
+++ Dtos/TokenDtos/TokenDto.cs
@@ -0,0 +1,13 @@
+﻿
+using Newtonsoft.Json;
+
+namespace Catalog_API.Dtos.TokenDtos
+{
+    public record TokenDto
+    {
+        public string AccessToken { get; init; }
+
+        public string RefreshToken { get; init; }
+    }
+}
+
