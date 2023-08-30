95e278ce3f8c81e0f0dcff650e5b35619e4c4539
Kene <kayyungx@gmail.com>
2023-08-30 16:27:10 +0100

    add drivers table migration

---
+++ Migrations/20230829123642_AddDriversTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDriversTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
