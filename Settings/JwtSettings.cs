d9a6b1422503651a33b3ade274e1fb6fa92ab973
Kene <kayyungx@gmail.com>
2023-08-30 16:29:57 +0100

    update jwt settings

--- Settings/JwtSettings.cs
+++ Settings/JwtSettings.cs
@@ -8,6 +8,7 @@ namespace Catalog_API.Settings
     public class JwtSettings
     {
         public string Key { get; set; }
+        public string RefreshTokenKey { get; set; }
         public string Issuer { get; set; }
     }
 }
