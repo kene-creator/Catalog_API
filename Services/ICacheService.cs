c808bfdc96d91faac94674327f0a9e72b1dc8487
Kene <kayyungx@gmail.com>
2023-08-31 21:13:40 +0100

    Add cache service interface

---
+++ Services/ICacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog_API.Services;
+
+public interface ICacheService
+{
+    T GetData<T>(string key);
+    bool SetData<T>(string key, T value, DateTimeOffset ttl);
+    object RemoveData(string key);
+}
