d497c849e4697daf32763e40491da152963b33b0
Kene <kayyungx@gmail.com>
2023-12-17 21:22:14 +0100

    update user entity model

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,22 +1,47 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Catalog_API.Enums;
 
 namespace Catalog_API.Entities
 {
     public record User
     {
+        [Key]
+        [Column("UserId")]
         public Guid Id { get; init; }
+
+        [Required(ErrorMessage = "CreatedAt is required.")]
         public DateTimeOffset CreatedAt { get; init; }
+
         public DateTime? UpdatedAt { get; init; }
-        public required string Email { get; init; }
-        public required string PasswordHash { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; init; }
+
+        [Required(ErrorMessage = "PasswordHash is required.")]
+        public string PasswordHash { get; set; }
+
         public string RefreshToken { get; set; } = string.Empty;
+
         public string? EmailToken { get; init; }
+
         public string? ResetToken { get; init; }
+
         public DateTime? ResetTokenExpiresAt { get; init; }
+
         public bool? EmailValid { get; init; }
-        public required string FirstName { get; init; }
-        public required string LastName { get; init; }
+
+        [Required(ErrorMessage = "FirstName is required.")]
+        public string FirstName { get; init; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        public string LastName { get; init; }
+
         public int? FailedSignInAttempts { get; init; }
-        public required UserRole[] Roles { get; init; }
+
+        [Required(ErrorMessage = "At least one role is required.")]
+        public UserRole[] Roles { get; init; }
     }
 }
+
